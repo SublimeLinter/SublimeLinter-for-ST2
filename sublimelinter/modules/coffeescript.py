ae80b4cbe4ba8019d2781d47e2d5e4c92847b0d5
José F. Romaniello <jfromaniello@gmail.com>
2011-11-24 23:43:26 -0300

    make the coffeescript linter work in windows with coffee.cmd (auto generated by npm)

--- sublimelinter/modules/coffeescript.py
+++ sublimelinter/modules/coffeescript.py
@@ -2,17 +2,33 @@
 # coffeescript.py - sublimelint package for checking coffee files
 
 import re
+import subprocess
+import itertools
 
 from base_linter import BaseLinter
 
 CONFIG = {
     'language': 'coffeescript',
-    'executable': 'coffee',
     'lint_args': '-l'
 }
 
 
 class Linter(BaseLinter):
+
+    def get_executable(self, view):
+        candidates = ["coffee", "coffee.cmd"]
+        for try_executable_result in itertools.imap(self.try_executable, candidates):
+            if try_executable_result[0]:
+                return try_executable_result
+        return (False, '', "coffee or coffee.cmd (windows) is required ")
+
+    def try_executable(self, executable):
+        try:
+            subprocess.call([executable, '-v'], startupinfo=self.get_startupinfo())
+            return (True, executable, '')
+        except OSError:
+            return (False, '', executable + ' is not present')
+
     def parse_errors(self, view, errors, lines, errorUnderlines,
                      violationUnderlines, warningUnderlines, errorMessages,
                      violationMessages, warningMessages):
