677df2547ac91a9ac9aaa3fb4807e5633451a751
Michael Dever <michael.dever2@mail.dcu.ie>
2012-01-24 23:19:44 +0000

    working more

--- sublimelinter/modules/haskell.py
+++ sublimelinter/modules/haskell.py
@@ -2,13 +2,15 @@
 # php.py - sublimelint package for checking php files
 
 import re
+import tempfile
 
 from base_linter import BaseLinter, INPUT_METHOD_FILE, INPUT_METHOD_TEMP_FILE
 
 CONFIG = {
     'language': 'haskell',
     'executable': 'hlint',
-    'input_method': INPUT_METHOD_FILE
+    'input_method': INPUT_METHOD_FILE,
+    'lint_args': '{filename}'
 }
 
 
@@ -16,17 +18,20 @@ class Linter(BaseLinter):
     def parse_errors(self, view, errors, lines, errorUnderlines, violationUnderlines, warningUnderlines, errorMessages, violationMessages, warningMessages):
         i = 0
         errorLines = errors.splitlines()
+        print len(errorLines)
         while i < len(errorLines):
+            #print errorLines[i] + " "
             match = re.match(r'^.+:(?P<line>\d+):\d+:.+:(?P<error>.+)', errorLines[i])
 
             if match:
                 error, line = match.group('error'), match.group('line')
+                print "error" + error + " " + line + "\n"
                 found = errorLines[i + 2].strip()
-                print found
+                #print found
 
-                lineno = int(line) - 1
+                lineno = int(line)
                 lines.add(lineno)
-                self.addMessage(lineno, error)
+                self.add_message(lineno, lines, error, errorMessages)
 
                 #reg = '(?P<underline>{0})'.format(re.escape(found)).replace(r"\ ", r"\ ?")
                 #self.underlineRegion(lineno, reg)
