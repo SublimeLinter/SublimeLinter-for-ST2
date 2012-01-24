5857d55d4e2ae8d0c31f4f104c5e2a1844fa1099
Michael Dever <michael.dever2@mail.dcu.ie>
2012-01-24 22:03:55 +0000

    work so far

---
+++ sublimelinter/modules/haskell.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# php.py - sublimelint package for checking php files
+
+import re
+
+from base_linter import BaseLinter, INPUT_METHOD_FILE, INPUT_METHOD_TEMP_FILE
+
+CONFIG = {
+    'language': 'haskell',
+    'executable': 'hlint',
+    'input_method': INPUT_METHOD_FILE
+}
+
+
+class Linter(BaseLinter):
+    def parse_errors(self, view, errors, lines, errorUnderlines, violationUnderlines, warningUnderlines, errorMessages, violationMessages, warningMessages):
+        i = 0
+        errorLines = errors.splitlines()
+        while i < len(errorLines):
+            match = re.match(r'^.+:(?P<line>\d+):\d+:.+:(?P<error>.+)', errorLines[i])
+
+            if match:
+                error, line = match.group('error'), match.group('line')
+                found = errorLines[i + 2].strip()
+                print found
+
+                lineno = int(line) - 1
+                lines.add(lineno)
+                self.addMessage(lineno, error)
+
+                #reg = '(?P<underline>{0})'.format(re.escape(found)).replace(r"\ ", r"\ ?")
+                #self.underlineRegion(lineno, reg)
+                i += 3
+
+            i += 1
