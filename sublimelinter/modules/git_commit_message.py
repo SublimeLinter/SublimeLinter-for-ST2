7d3d69c24b2912cc30d1bc80f7bcdc741db8cb6b
Tristan Harward <trisweb@gmail.com>
2012-07-20 12:08:58 -0400

    Reversed the length check & changed error types.
    Length check won't work if it always hits 50 first! Reverse the
    order so it checks for 68 first, then 50.

--- sublimelinter/modules/git_commit_message.py
+++ sublimelinter/modules/git_commit_message.py
@@ -40,22 +40,23 @@ class Linter(BaseLinter):
                 if line.strip():
                     first_line_of_message = lineno
 
-                    if len(line) > 50:
+                    if len(line) > 68:
+                        errors.append({
+                            'type': ErrorType.ERROR,
+                            'message': 'Subject line must be 68 characters or less (github will truncate).',
+                            'lineno': real_lineno,
+                            'col': 68,
+                        })
+                    elif len(line) > 50:
                         errors.append({
                             'type': ErrorType.WARNING,
                             'message': 'Subject line should be 50 characters or less.',
                             'lineno': real_lineno,
                             'col': 50,
                         })
-                    elif len(line) > 68:
-                        errors.append({
-                            'type': ErrorType.VIOLATION,
-                            'message': 'Subject line must be 68 characters or less (github will truncate).',
-                            'lineno': real_lineno,
-                            'col': 68,
-                        })
                     elif lineno != 1:
                         errors.append({
+                            'type': ErrorType.ERROR,
                             'message': 'Subject must be on first line.',
                             'lineno': real_lineno,
                         })
